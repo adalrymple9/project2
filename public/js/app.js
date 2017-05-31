d133d5460f4711ba27d41025efb7307244b93ad5
Mark Martinez <marksmusic29@gmail.com>
2017-05-30 20:54:55 -0400

    update files

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,5 @@
-//logic for home
+//-----------------logic for home----------------------//
+
 	//loads sign up form when register button is pressed
 	$('#p2-registerNow').on('click', function() {
 	    event.preventDefault();
@@ -8,7 +9,13 @@
 	    $('#p2-registerNow').toggle();
 	    $('#p2-registration').addClass('fadeIn animated');
 	});
-
+	//clears error mess from the sign up when a key is pressed
+	$('#p2-password').keyup(function() {
+	    $('#p2-signUpMess').text(' ')
+	});
+	$('#p2-confirmPassword').keyup(function() {
+	    $('#p2-signUpMess').text(' ')
+	});
 	//collects data from sign up form when submit button is pressed
 	$('body').on('click', '#p2-crateProfile', function() {
 	    event.preventDefault();
@@ -23,23 +30,24 @@
 	        };
 	    } else {
 	        //using alert for now will make a tooltip after
-	        alert('Sorry the passwords dont match.');
+	        $('#p2-signUpMess').text('Sorry the passwords dont match.');
 	    }
 	    console.log(newUser);
 	});
 
 	//collects data from log in form when the log in button is pressed
-	$('#p2-logIn').on('click', function(){
-		event.preventDefault();
-		var userName = $().val();
-		var password = $().val();
-		var user = {userName: userName, password: password};
-		//send data to data base to compair the values
-		$.post(
-			//if true change loged in state to true and guide to profile page
-			//else keep loged in state at false and prompt user to check info
-		);
+	$('#p2-logIn').on('click', function() {
+	    event.preventDefault();
+	    var userName = $().val();
+	    var password = $().val();
+	    var user = { userName: userName, password: password };
+	    //send data to data base to compair the values
+	    $.post(
+	        //if true change loged in state to true and guide to profile page
+	        //else keep loged in state at false and prompt user to check info
+	    );
 	});
-//logic for profile
+//-------------------logic for profile----------------------//
 
-//logig for trip
+
+//--------------------logig for trip------------------------//
--- public/js/style.js
+++ public/js/style.js
@@ -2,10 +2,14 @@
 $('#p2-homeContainer').css('height', $(window).height());
 
 //get new backgound
-$.get('https://pixabay.com/api/videos/?key=5042844-4a24d0bc1db1dad1f577233ff&q=city' )
+$.get('https://pixabay.com/api/?key=5042844-4a24d0bc1db1dad1f577233ff&q=travel' )
 .done(function(results){
-	var video = results.hits[0].videos.large.url;
-$('#p2-homeVideo source').attr('src', video);
-
+	console.log(results.hits[0].webformatURL);
+	var image = results.hits[9].webformatURL;
+	$('#p2-homeContainer').css('background-image', 'url('+image+')');
+//	var video = results.hits[0].videos.large.url;
+//	$('#p2-homeVideo').attr('autoplay');
+//$('#p2-homeVideo source').attr('src', video);
+//
 
 });
