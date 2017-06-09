77a60b07c6c44fe50b1d2714df311c1d032d3989
Andrea Dalrymple <andrea.dalrymple@yahoo.com>
2017-06-09 19:19:30 -0400

    removes loadash

--- connections/api.js
+++ connections/api.js
@@ -1,21 +1,3 @@
-// Load the full build. 
-var _ = require('lodash');
-// Load the core build. 
-var _ = require('lodash/core');
-// Load the FP build for immutable auto-curried iteratee-first data-last methods. 
-var fp = require('lodash/fp');
-
-var request = require('request');
-
-
-// Load method categories. 
-var array = require('lodash/array');
-var object = require('lodash/fp/object');
-
-// Cherry-pick methods for smaller browserify/rollup/webpack bundles. 
-var at = require('lodash/at');
-var curryN = require('lodash/fp/curryN');
-
 var photoUrl;
 
 var venueId;
